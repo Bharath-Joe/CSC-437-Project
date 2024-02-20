9e331c4b1569c6adb905e28eeba3c6e716129eb4
Bharath-Joe <Bharath.r.senthilkumar@gmail.com>
2024-02-20 12:51:48 -0800

    Started project

--- packages/lit-frontend/src/header-component.ts
+++ packages/lit-frontend/src/header-component.ts
@@ -1,25 +1,69 @@
 import { css, html, LitElement } from "lit";
-import { customElement } from "lit/decorators.js";
+import { customElement, property, state } from "lit/decorators.js";
 
 @customElement("header-component")
 class HeaderElement extends LitElement {
+    @property()
+    currentPage = "dashboard";
+
+    handleNavigation(page: string) {
+        // Update the currentPage property when navigating to a different page
+        this.currentPage = page;
+    }
+
     render() {
         return html` <header class="Website-Side-Header">
             <section class="Website-Title">
                 <h1>MealMaker</h1>
+                <img src="../images/headshot.jpg" />
+                <p>Bharath Senthilkumar</p>
             </section>
             <section class="Side-Header-Contents">
                 <ul>
-                    <li>Recipies</li>
-                    <li>Favorites</li>
-                    <li>Filters</li>
-                    <li>Create</li>
+                    <li>
+                        <a href="index.html">
+                            <svg class="icon">
+                                <use href="/icons/icons.svg#dashboard" /></svg
+                            >Dashboard
+                        </a>
+                    </li>
+                    <li>
+                        <a href="favorites.html">
+                            <svg class="icon">
+                                <use href="/icons/icons.svg#favorite" /></svg
+                            >Favorites
+                        </a>
+                    </li>
+                    <li>
+                        <a href="filters.html">
+                            <svg class="icon">
+                                <use href="/icons/icons.svg#filter" /></svg
+                            >Filters
+                        </a>
+                    </li>
+                    <li>
+                        <a href="create.html">
+                            <svg class="icon">
+                                <use href="/icons/icons.svg#create" /></svg
+                            >Create
+                        </a>
+                    </li>
                 </ul>
             </section>
             <section>
                 <ul>
-                    <li>Settings</li>
-                    <li>Log Out</li>
+                    <li>
+                        <a href="settings.html">
+                            <svg class="icon">
+                                <use href="/icons/icons.svg#settings" /></svg
+                            >Settings
+                        </a>
+                    </li>
+                    <li class="logout">
+                        <svg class="icon">
+                            <use href="/icons/icons.svg#logout" /></svg
+                        >Log Out
+                    </li>
                 </ul>
             </section>
         </header>`;
@@ -33,20 +77,70 @@ class HeaderElement extends LitElement {
 
         h1 {
             font-family: var(--font-family-header);
-            font-size: 
+            font-size: var(--size-spacing-large);
+        }
+
+        .Website-Title {
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: var(--size-spacing-small);
         }
 
         .Website-Side-Header {
-            padding: 40px 50px 40px 50px;
+            padding: 30px;
             display: flex;
-            color: white;
-            // border: 1px solid black;
+            color: var(--color-text);
             border-radius: 0px 30px 0px 0px;
             background-color: var(--color-background);
-            width: fit-content;
             height: 100vh;
+            width: 250px;
             flex-direction: column;
             justify-content: space-between;
+        }
+
+        ul {
+            display: flex;
+            flex-direction: column;
+            list-style: none;
+            gap: var(--size-spacing-small);
+            margin: 0;
+            padding: 0;
+        }
+
+        li a,
+        .logout {
+            text-decoration: none;
+            color: var(--color-text);
+            display: flex;
+            align-items: center;
+            gap: var(--size-spacing-small);
+            font-size: var(--size-spacing-small);
+            padding: 8px 8px 8px 8px;
+        }
+
+        li a.active {
+            background-color: var(--color-background-invert);
+            color: var(--color-text-invert);
+            border-radius: 10px;
+        }
+
+        img {
+            border-radius: 50%;
+            width: 100px;
+            height: 100px;
+            object-fit: cover;
+            object-position: center;
+            overflow: hidden;
+        }
+
+        .icon {
+            display: inline;
+            height: 1.5em;
+            width: 1.5em;
+            vertical-align: top;
+            fill: currentColor;
+            stroke: currentColor;
         }
     `;
 }
--- packages/lit-frontend/src/meal-card.ts
+++ packages/lit-frontend/src/meal-card.ts
@@ -4,41 +4,64 @@ import { customElement, property } from "lit/decorators.js";
 @customElement("meal-card")
 class MealElement extends LitElement {
     @property({ type: String })
-    htmlFile: string = "";
-
-    @property({ type: String })
     src: string = "";
 
     render() {
         return html`
             <li class="card">
-                <a href="${this.htmlFile}" class="card-contents">
-                    <img src="${this.src}" width="200" />
-                    <hr width="100%" />
-                    <slot></slot>
-                </a>
+                <section class="card-contents">
+                    <section class="card-header">
+                        <slot></slot>
+                        <svg class="icon">
+                            <use href="/icons/icons.svg#add" />
+                        </svg>
+                    </section>
+                    <img src="${this.src}" />
+                </section>
             </li>
         `;
     }
 
     static styles = css`
+
+        img {
+            width: 275px;
+            height: 275px;
+            border-radius: 10px;
+            overflow: hidden;
+            object-fit: cover;
+        }
         .card {
+            padding: 15px;
             border: 2px solid var(--color-border);
             border-radius: 10px;
-            padding: 15px;
-            color: var(--color-text);
-        }
-        .card:hover {
-            background-color: var(--color-hover);
+            color: var(--color-body-text);
+            font-family: var(--font-family-header);
+            width: 275px;
         }
         .card-contents {
             display: flex;
             flex-direction: column;
-            align-items: center;
+            gap: var(--size-spacing-large);
+        }
+
+        .card-header {
+            display: flex;
+            align-items: baseline;
+            justify-content: space-between;
             gap: var(--size-spacing-medium);
-            color: var(--color-text);
-            text-decoration: none;
+            font-size: 1.5rem;
             font-weight: bold;
+            height: 50px;
+        }
+
+        svg.icon {
+            display: inline;
+            height: 1.5em;
+            width: 1.5em;
+            vertical-align: top;
+            fill: var(--color-background);
+            stroke: var(--color-background);
         }
     `;
 }
